9578eed2d87d5bf7b4f8f513c06ab852c64ad307
baiest <juanballesteros2001@gmail.com>
2022-05-03 20:26:07 -0500

    Add: funtion to separate sets

--- conjuntos.php
+++ conjuntos.php
@@ -2,5 +2,24 @@
 
 function split_sets($array)
 {
-  echo $array;
+  #Convertir arreglo a string
+  $str = implode("", $array);
+
+  #Verificar vacios inicio y fin
+  if ($array[0] == 0) $str = "0" . $str;
+  if ($array[count($array) - 1] == 0) $str = $str . "0";
+
+  #Remplazar vacios con X
+  $new_str = str_replace("00", "0X0", $str);
+  $sets = explode("0", $new_str);
+
+  #Limpiar inicio y fin del arreglo
+  if (!$sets[0]) array_shift($sets);
+  if (!$sets[count($sets) - 1]) array_pop($sets);
+
+  foreach ($sets as $items) {
+    $newArray = str_split($items);
+    sort($newArray);
+    echo implode("", $newArray) . " ";
+  }
 }
--- main.php
+++ main.php
@@ -1,5 +1,8 @@
 <?php
 include 'matriz_x.php';
 include 'conjuntos.php';
-// matrizX(20);
+
+matrizX(20);
 split_sets([1, 3, 2, 0, 7, 8, 1, 3, 0, 6, 7, 1]);
+echo "\n";
+split_sets([2, 1, 0, 0, 3, 4]);
